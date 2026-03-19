aee192fbfe0196e5e1e21b6d1a1b48f57eb5c4e8
oshan-navishka <oshannavishka1234@gmail.com>
2026-03-19 16:08:33 +0530

    js-1

---
+++ 03_JS/script.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log("Hello!");
+console.log("Bye!");
+
+/*methana hadala adala file ekata dhn script kiyala ekaka hadala ek athule meke parth eka denna oni
+            ex:- <script src="script.js"></script>*/
