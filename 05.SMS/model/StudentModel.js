81840cd9543d2f1ecc3a2d5953e0b3e93519ae76
oshan-navishka <oshannavishka1234@gmail.com>
2026-04-23 16:33:37 +0530

    update sms file

--- 05.SMS/model/StudentModel.js
+++ 05.SMS/model/StudentModel.js
@@ -1,14 +1,69 @@
 import { student_db } from "../db/db.js";
 
+
+
+class Student {
+    #id;
+    #name;
+    #nic
+    #phone;
+    #address;
+
+    constructor(id, name, nic, phone, address) {
+        this.#id = id;
+        this.#name = name;
+        this.#nic = nic;
+        this.#phone = phone;
+        this.#address = address;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get nic() {
+        return this.#nic;
+    }
+
+    get phone() {
+        return this.#phone;
+    }
+
+    get address() {
+        return this.#address;
+    }
+
+    set id(newId) {
+        this.#id = newId;
+    }
+
+    set name(newName) {
+        this.#name = newName;
+    }
+
+    set nic(newNic) {
+        this.#nic = newNic;
+    }
+
+    set phone(newPhone) {
+        this.#phone = newPhone;
+    }
+
+    set address(newAddress) {
+        this.#address = newAddress;
+    }
+
+}
+
+
+
 // ------------------------------ Add Student -------------------------------
 const addStudentData = (studentId, studentName, studentNic, studentPhone, studentAddress) => {
-    const new_student = {
-        id: studentId,
-        name: studentName,
-        nic: studentNic,
-        phone: studentPhone,
-        address: studentAddress
-    };
+    let new_student = new Student(studentId, studentName, studentNic, studentPhone, studentAddress);
 
     student_db.push(new_student);
     return new_student;
